1b987ec2d234f1e5ea4b61de9ddacdb85b750884
Mike Crockett <you@somedomain.com>
2014-09-10 14:10:14 -0500

    Adding helper class to convert arrays to the necessary post-style values for Redcap.

--- lib/RedcapAPI.rb
+++ lib/RedcapAPI.rb
@@ -1,6 +1,24 @@
 require "RedcapAPI/version"
 require "json"
 require "mechanize"
+  class Hash
+    def to_http_post_array
+      r = {}
+
+      self.each_key do |k|
+        if (false == self[k].is_a?(Array))
+          raise "!ERROR: Expecting hash value to be an Array."
+        end
+
+        self[k].each_with_index do |v,i|
+          r["#{k}[#{i}]"] = v
+        end
+      end
+
+      return r
+    end
+  end
+
   class RedcapAPI
     DEFAULT_PARAMS = {
       :content => 'record',
